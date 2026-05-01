01dcf38474923c560cdfac74d304dc84b938802b
pawarsomya2006-blip <pawarsomya2006@gmail.com>
2026-05-01 21:50:51 +0530

    Update Calculature.cpp

--- TASK1/Calculature.cpp
+++ TASK1/Calculature.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a,b;
+    float a, b;  // int ki jagah float kar diya
     char op;
-cout<<"Enter first Number:";
-    cin>>a;
-cout<<"Enter second number:";
-cin>>b;
-cout<<"Enter operator(+,-,*,/):";
-cin>>op;
-if(op=='+')
-cout<<"result="<<(a+b)<<endl;
-else if(op=='-')
-cout<<"result="<<(a-b)<<endl;
-else if(op=='*')
-cout<<"result="<<(a*b)<<endl;
-else if(op=='/')
-cout<<"result"<<(a/b)<<endl;
-else 
-cout<<"error";
-return 0;
+    
+    cout << "Enter first Number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Enter operator(+,-,*,/): ";
+    cin >> op;
+    
+    if(op == '+')
+        cout << "Result = " << (a+b) << endl;
+    else if(op == '-')
+        cout << "Result = " << (a-b) << endl;
+    else if(op == '*')
+        cout << "Result = " << (a*b) << endl;
+    else if(op == '/') {
+        if(b == 0)  // Zero se divide check kar liya
+            cout << "Error: Cannot divide by zero!" << endl;
+        else
+            cout << "Result = " << (a/b) << endl;
+    }
+    else
+        cout << "Error: Invalid operator!" << endl;
+    
+    return 0;
+}
 }
